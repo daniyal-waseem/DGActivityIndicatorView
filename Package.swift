ff220023411ef03064bddfcd85077a1fba562142
daniyal-waseem <82135420+daniyal-waseem@users.noreply.github.com>
2025-10-17 10:51:06 +0100

    Initialize Package.swift for DGActivityIndicatorView

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.6
+import PackageDescription
+
+let package = Package(
+    name: "DGActivityIndicatorView",
+    platforms: [.iOS(.v11)],
+    products: [
+        .library(
+            name: "DGActivityIndicatorView",
+            targets: ["DGActivityIndicatorView"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "DGActivityIndicatorView",
+            path: "DGActivityIndicatorView",
+            publicHeadersPath: "."
+        )
+    ],
+    swiftLanguageVersions: [.v5]
+)
