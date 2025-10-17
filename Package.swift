0d27576795163ee8929f43a61256a38955a954da
daniyal-waseem <82135420+daniyal-waseem@users.noreply.github.com>
2025-10-17 12:08:34 +0100

    Fix formatting in Package.swift

--- Package.swift
+++ Package.swift
@@ -3,23 +3,17 @@ import PackageDescription
 
 let package = Package(
     name: "DGActivityIndicatorView",
-    platforms: [
-        .iOS(.v11)
-    ],
+    platforms: [.iOS(.v11)],
     products: [
         .library(
             name: "DGActivityIndicatorView",
             targets: ["DGActivityIndicatorView"]
-        )
+        ),
     ],
     targets: [
         .target(
             name: "DGActivityIndicatorView",
-            path: "DGActivityIndicatorView",
-            publicHeadersPath: ".",
-            cSettings: [
-                .headerSearchPath(".")
-            ]
+            path: "DGActivityIndicatorView"
         )
     ],
     swiftLanguageVersions: [.v5]
